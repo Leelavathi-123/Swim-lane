b7ff17336c271320a911b4712f63340316d427aa
Leelavathi N <leelavathinarasimha1@gmail.com>
2024-06-03 06:19:09 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+
+# I'm excited to share this movie swim lane component I built using Vue.js and the Swiper library! This README will guide you through its functionality,  and how to get it up and running.
+
+## What It Does:
+-This sleek component showcases a horizontal slider of movie titles and their corresponding posters.
+-It leverages the power of Swiper to provide smooth transitions and user-friendly navigation between slides.
+-It's built with responsiveness in mind, automatically adjusting the layout for different screen sizes, ensuring a great user experience on all devices.
+-Enhanced User Interaction: The slider displays a clear indication of the currently selected movie, providing visual feedback to users and enabling them to easily make informed choices.
+-it will  emphasizes the user experience benefits by highlighting navigation controls and active slide indication. It clarifies how users can interact with the slider and understand their current selection.
+
+# How to Run this Code
+
+To run this code, please follow the steps below:
+
+- Clone the project from GitHub or from the google drive 
+- Open your terminal using the shortcut `Ctrl + Shift + `.
+- Use the following commands to set up and run the code:
+
+npm install 
+
+npm run dev
+
+## code structure
+-App.vue:   The main application component that renders the movie slider.
+-main.js:   Configures the Vue application and router.
+-style.css : Defines styles for the slider and its components. 
---
+++ src/App.vue
@@ -0,0 +1,87 @@
+<template>
+<div class="main-div"><h1>Most-watched films</h1></div>
+  <swiper
+    :cssMode="true"
+    :slidesPerView="6.5"
+    :spaceBetween="20"
+    :navigation="true"
+    :pagination="{
+      clickable: true,
+    }"
+    :mousewheel="true"
+    :keyboard="true"
+    :loop="true"
+    :history="{
+      key: 'slide',
+    }"
+  :breakpoints="{
+  '320': {
+    slidesPerView: 1,
+  },
+  '488': {
+    slidesPerView: 2 ,
+  },
+  '768': {
+    slidesPerView: 3,  
+    spaceBetween:10,  
+  },
+  '1200':{
+    slidesPerView: 6.5, 
+    spaceBetween:20, 
+  }
+  }"
+    :modules="modules"
+    class="mySwiper"
+  >
+     <swiper-slide
+      v-for="(movie, index) in movies"
+      :key="index"
+      :data-history="movie.history"
+    >
+      <div class="slide-content">
+        <img v-bind:src="movie.img" />
+        <div v-if="movie.new" class="new-button">New</div>
+        <h1>{{ movie.title }}</h1>
+      </div>
+    </swiper-slide>
+  </swiper>
+</template>
+<script>
+
+  import { Swiper, SwiperSlide } from 'swiper/vue';
+  import 'swiper/css';
+  import 'swiper/css/navigation';
+  import 'swiper/css/pagination';
+
+  import './style.css';
+  import { Navigation, Pagination, History, Mousewheel, Keyboard } from 'swiper/modules';
+
+  export default (await import('vue')).defineComponent({
+    components: {
+      Swiper,
+      SwiperSlide,
+    },
+    setup() {
+      const movies = [
+        { history: 1, img:('../src/assets/i1.png'), title: 'Oppenheimer' },
+        { history: 2, img: '../src/assets/i2.png', title: 'Past Lives' },
+        { history: 3, img: '../src/assets/i3.png', title: 'Polite Society' },
+        { history: 4, img: '../src/assets/i4.png', title: 'Robots' },
+        { history: 5, img: '../src/assets/i5.png', title: 'Sanctuary' },
+        { history: 6, img: '../src/assets/i6.png', title: 'Talk To Me' },
+        { history: 7, img: '../src/assets/i7.png', title: 'About My Father', new: true },
+        { history: 8, img: '../src/assets/i8.png', title: 'Air' },
+        { history: 9, img: '../src/assets/i9.png', title: 'Are You There God? It’s Me, Margaret.', new: true },
+        { history: 10, img: '../src/assets/i10.png', title: 'Guardians of the Galaxy: Volume 3' },
+        { history: 11, img: '../src/assets/i11.png', title: 'Big George Foreman' },
+        { history: 12, img: '../src/assets/i12.png', title: 'Beau Is Afraid' },
+      ];
+
+      return {
+        movies,
+        modules: [Navigation, Pagination, History, Mousewheel, Keyboard],
+      };
+    },
+  });
+</script>
+
